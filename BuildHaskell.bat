23a18597e896d176b713b9d0a8a24d1494b28ef4
TytanRock <nesscory@gmail.com>
2019-04-22 14:00:24 -0400

    Modified haskell build to match c build

--- BuildHaskell.bat
+++ BuildHaskell.bat
@@ -9,7 +9,7 @@ if NOT EXIST %1\bin\ (
     mkdir %1\bin\
     echo Making Bin Directory
 )
-ghc %1\%2 -outputdir %1\build\ -o %1\bin\main.exe
+ghc %1\%2 -outputdir %1\bin\ -o %1\build\main.exe
 
 if %ERRORLEVEL% NEQ 0 (
     echo Build Errors detected 
