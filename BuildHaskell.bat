236d7bcec9d911beaae0f142da13375188ce60e4
TytanRock <nesscory@gmail.com>
2019-04-22 14:32:44 -0400

    Added make flag to ghc compiler

--- BuildHaskell.bat
+++ BuildHaskell.bat
@@ -9,7 +9,7 @@ if NOT EXIST %1\bin\ (
     mkdir %1\bin\
     echo Making Bin Directory
 )
-ghc %1\%2 -outputdir %1\bin\ -o %1\build\main.exe
+ghc --make %1\%2 -outputdir %1\bin\ -o %1\build\main.exe
 
 if %ERRORLEVEL% NEQ 0 (
     echo Build Errors detected 
