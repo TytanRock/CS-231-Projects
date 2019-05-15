4bc1eea62a8d9993e59c0219986e1305bd053036
TytanRock <nesscory@gmail.com>
2019-05-15 15:49:34 -0400

    SpellCheck in Haskell

---
+++ _7SpellCheckHaskell/spellCheck.hs
@@ -0,0 +1,58 @@
+module Main where
+import Data.Char
+import Data.List
+import System.Environment
+import System.IO
+
+getWords :: String -> [String]
+getWords [] = []
+getWords [a] = [[a]]
+getWords (a:b:abs) 
+    | not $ isAlpha a = getWords (b:abs)
+    | otherwise = do
+        if isAlpha b then do
+            let remainder = getWords (b : abs)
+            [a : (remainder !! 0)] ++ tail remainder
+        else
+            [[a]] ++ getWords abs
+
+removeLikeWords :: [String] -> [String]
+removeLikeWords [] = []
+removeLikeWords (a:as)
+    | elem (map toLower a) (map (map toLower) as) = removeLikeWords as
+    | otherwise = a : removeLikeWords as
+
+caseInsensitiveOrdering :: String -> String -> Ordering
+caseInsensitiveOrdering a b
+    | (map toLower a) < (map toLower b) = LT
+    | otherwise = GT
+
+processWords :: String -> [String]
+processWords str = sortBy (caseInsensitiveOrdering) (removeLikeWords $ getWords str)
+
+checkSpelling :: [String] -> [String] -> [String]
+checkSpelling [] _ = []
+checkSpelling (str:strs) dic
+    | elem (map toLower str) (map (map toLower) dic) = (str ++ " is correctly spelled") : checkSpelling strs dic
+    | otherwise = (str ++ " is not correctly spelled") : checkSpelling strs dic
+
+main = do
+    [dictionaryFile, inFile, outFile] <- getArgs
+    dictionaryHandle <- openFile dictionaryFile ReadMode
+    inFileHandle <- openFile inFile ReadMode
+    outFileHandle <- openFile outFile WriteMode
+
+    inFileContents <- hGetContents inFileHandle
+    dicFileContents <- hGetContents dictionaryHandle
+
+    let allWords = processWords inFileContents
+    let spellCheckedWords = checkSpelling allWords $ processWords dicFileContents
+    
+    hPutStrLn outFileHandle $ unlines spellCheckedWords
+
+    hClose dictionaryHandle
+    hClose inFileHandle
+    hClose outFileHandle
+
+    putStrLn "done"
+    
